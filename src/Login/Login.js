b5acc46abcfb1daf4607a116809891b81ffef880
M.umer <umerprogrammer@gmail.com>
2022-04-28 23:14:26 +0500

    I added some dapp  functionality

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,38 +1,18 @@
 import React from 'react'
 import { useNewMoralisObject } from "react-moralis";
-import { useMoralisQuery } from "react-moralis";
+import { useMoralisQuery } from "react-moralis"
 
 
 
 export default function Login() {
-    const { save } = useNewMoralisObject("EthTransactions");
+    const { save } = useNewMoralisObject("Username");
+    const { data, error, isLoading } = useMoralisQuery("Username");
 
-
-  const { fetch } = useMoralisQuery(
-    "Username",
-    (query) => query.equalTo("username", "Umer"),
-    [],
-    { autoFetch: false }
-  );
-
-
-
-const singleQuery = () =>{
-    fetch({
-      onSuccess: (result) => console.log(result),
-      onError: (error) => {
-          
-        alert("Failed to create new object, with error code: " + error.message);
-      },
-    });
-}
-   
-
-    const sumbit = ()=>{
-        const data = {
-            email: 'umer@gmail.com',
-            username: ' Umer'
-          
+  const sumbit = ()=>{
+    const data = {
+      email: 'umer@gmail.com',
+      username: ' Umer'
+      
           };
 
           save(data, {
@@ -40,16 +20,26 @@ const singleQuery = () =>{
               alert("New object created with objectId: " + obj.id);
             },
             onError: (error) => {
-          
+              
               alert("Failed to create new object, with error code: " + error.message);
             },
           });
-    }
-  return (
-    <div>
+        }
+      
 
-        <button onClick={sumbit}>Click</button>
-        <button onClick={singleQuery}>Call The Code</button>
-    </div>
-  )
+
+        if (error) {
+          return <span>🤯</span>;
+        }
+        
+        if (isLoading) {
+          return <span>🙄</span>;
+        }
+        
+        return (
+        <>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        </>
+        )
+
 }
